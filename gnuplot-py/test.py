3cd91810f816951272ab64f89c040280dce8eead
mhagger <mhagger@cc615063-760d-0410-8897-8c88c9ce4bef>
1999-07-23 18:17:21 +0000

    Wrote most tests for File.

--- gnuplot-py/test.py
+++ gnuplot-py/test.py
@@ -20,52 +20,82 @@ Boston, MA 02111-1307, USA.
 
 __cvs_version__ = 'CVS version $Revision$'
 
-import tempfile
+import sys, math
 from Numeric import *
 import Gnuplot
-from Gnuplot import write_array, grid_function
-from Gnuplot import AnyFile, TempFile, ArrayFile, TempArrayFile
-from Gnuplot import PlotItem, Func, File, Data, GridData
+gp = Gnuplot # abbreviation
+
+def wait():
+    raw_input('Please press return...\n')
+
 
 def main():
     """Exercise the Gnuplot module."""
 
-    g = Gnuplot.Gnuplot()
+    # Make a temporary file:
+    file1 = gp.TempFile() # will be deleted upon exit
+    f = open(file1.filename, 'w')
+    for x in arange(100)/5. - 10.:
+        f.write('%s %s %s\n' % (x, math.cos(x), math.sin(x)))
+    f.close()
+
+    g = gp.Gnuplot()
+    print 'Generate a File from a filename'
+    g.plot(gp.File(file1.filename))
+    wait()
+    print 'Generate a File given a TempFile object'
+    g.plot(gp.File(file1))
+    wait()
+
     g.title('Title')
-    g('set data style linespoints')
-    g.plot([[0,1.1], [1,5.8], [2,3.3], [3,4.2]])
+    g.xlabel('x')
+    g.ylabel('y')
+    print 'Set title and axis labels'
+    g.replot()
+    wait()
 
-    # Plot one dataset from an array and one via a gnuplot function;
-    # also demonstrate the use of item-specific options:
-    g2 = Gnuplot(debug=1)
-    x = arange(10, typecode=Float)
-    y1 = x**2
-    # Notice how this plotitem is created here but used later?  This
-    # is convenient if the same dataset has to be plotted multiple
-    # times.  It is also more efficient because the data need only be
-    # written to a temporary file once.
-    d = Data(x, y1,
-             title='calculated by python',
-             with='points 3 3')
-    g2.title('Data can be computed by python or gnuplot')
-    g2.xlabel('x')
-    g2.ylabel('x squared')
-    # Plot a function alongside the Data PlotItem defined above:
-    g2.plot(Func('x**2', title='calculated by gnuplot'), d)
+    print 'Style lines'
+    g.plot(gp.File(file1.filename, with='lines'))
+    wait()
+    print 'using=1, using=(1,)'
+    g.plot(gp.File(file1.filename, using=1, with='lines'),
+           gp.File(file1.filename, using=(1,), with='points'))
+    wait()
+    print 'using=(1,2), using="1:3"'
+    g.plot(gp.File(file1.filename, using=(1,2)),
+           gp.File(file1.filename, using='1:3'))
+    wait()
+    print 'title=None'
+    g.plot(gp.File(file1.filename, title=None))
+    wait()
+    print 'title="title"'
+    g.plot(gp.File(file1.filename, title='title'))
+    wait()
 
-    # Save what we just plotted as a color postscript file.
+    print 'Test changing File attributes after construction:'
+    f = gp.File(file1.filename)
+    print 'Original'
+    g.plot(f)
+    wait()
+    print 'Style linespoints'
+    f.set_option(with='linespoints')
+    g.plot(f)
+    wait()
+    print 'using=(1,3)'
+    f.set_option(using=(1,3))
+    g.plot(f)
+    wait()
+    print 'title=None'
+    f.set_option(title=None)
+    g.plot(f)
+    wait()
 
-    # With the enhanced postscript option, it is possible to show `x
-    # squared' with a superscript (plus much, much more; see `help set
-    # term postscript' in the gnuplot docs).  If your gnuplot doesn't
-    # support enhanced mode, set `enhanced=0' below.
-    g2.ylabel('x^2') # take advantage of enhanced postscript mode
+    g.ylabel('x^2') # take advantage of enhanced postscript mode
     print ('\n******** Generating postscript file '
            '"gnuplot_test_plot.ps" ********\n')
-    g2.hardcopy('gnuplot_test_plot.ps', enhanced=1, color=1)
+    g.hardcopy('gnuplot_test_plot.ps', enhanced=1, color=1)
 
     # Demonstrate a 3-d plot:
-    g3 = Gnuplot(debug=1)
     # set up x and y values at which the function will be tabulated:
     x = arange(35)/2.0
     y = arange(30)/10.0 - 1.5
@@ -75,12 +105,12 @@ def main():
     xm = x[:,NewAxis]
     ym = y[NewAxis,:]
     m = (sin(xm) + 0.1*xm) - ym**2
-    g3('set parametric')
-    g3('set data style lines')
-    g3('set hidden')
-    g3('set contour base')
-    g3.xlabel('x')
-    g3.ylabel('y')
+    g('set parametric')
+    g('set data style lines')
+    g('set hidden')
+    g('set contour base')
+    g.xlabel('x')
+    g.ylabel('y')
     # The `binary=1' option would cause communication with gnuplot to
     # be in binary format, which is considerably faster and uses less
     # disk space.  (This only works with the splot command due to
@@ -88,31 +118,12 @@ def main():
     # disable binary because older versions of gnuplot don't allow
     # binary data.  Change this to `binary=1' (or omit the binary
     # option) to get the advantage of binary format.
-    g3.splot(GridData(m,x,y, binary=0))
+    g.splot(gp.GridData(m,x,y, binary=0))
 
     # Delay so the user can see the plots:
     sys.stderr.write('Three plots should have appeared on your screen '
-                     '(they may be overlapping).\n'
-                     'Please press return to continue...\n')
-    sys.stdin.readline()
-
-    # ensure processes and temporary files are cleaned up:
-    del g1, g2, g3, d
-
-    # Enable the following code to test the old-style gnuplot interface
-    if 0:
-        # List of (x, y) pairs
-        plot([(0.,1),(1.,5),(2.,3),(3.,4)])
-
-        # List of y values, file output
-        print '\n            Generating postscript file "gnuplot_test2.ps"\n'
-        plot([1, 5, 3, 4], file='gnuplot_test2.ps')
-
-        # Two plots; each given by a 2d array
-        x = arange(10, typecode=Float)
-        y1 = x**2
-        y2 = (10-x)**2
-        plot(transpose(array([x, y1])), transpose(array([x, y2])))
+                     '(they may be overlapping).\n')
+    wait()
 
 
 if __name__ == '__main__':
