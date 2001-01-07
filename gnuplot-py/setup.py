d1c63b9de5419ec8e332047d9c0dbbb8745a9b78
mhagger <mhagger@cc615063-760d-0410-8897-8c88c9ce4bef>
2001-01-07 04:13:18 +0000

    Slight modifications to setup.py.
    Removed comments from MANIFEST so that it can be used by distutils.
    
    Changed version number to 1.5b1.

--- gnuplot-py/setup.py
+++ gnuplot-py/setup.py
@@ -1,21 +1,40 @@
 #! /usr/bin/env python
 
-"""Setup script for the Gnuplot module distribution."""
+"""Setup script for the Gnuplot module distribution.
 
-__revision__ = "$Id$"
+Copyright (C) 2001 Michael Haggerty <mhagger@alum.mit.edu>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at
+your option) any later version.  This program is distributed in the
+hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the GNU General Public License for more details; it is
+available at <http://www.fsf.org/copyleft/gpl.html>, or by writing to
+the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+"""
+
+__cvs_version__ = '$Revision$'
 
 from distutils.core import setup
 
-setup (# Distribution meta-data
-    name = "Gnuplot",
-    version = "1.4",
-    description = "A pipe-based Python interface to the gnuplot plotting program.",
-    author = "Michael Haggerty",
-    author_email = "mhagger@blizzard.harvard.edu",
-    url = "http://monsoon.harvard.edu/~mhagger/Gnuplot/Gnuplot.html",
+# Get the version number from the __init__ file:
+from __init__ import __version__
+
+setup (
+    # Distribution meta-data
+    name='Gnuplot',
+    version=__version__,
+    description='A Python interface to the gnuplot plotting program.',
+    author='Michael Haggerty',
+    author_email='mhagger@alum.mit.edu',
+    url='http://monsoon.harvard.edu/~mhagger/Gnuplot/Gnuplot.html',
 
     # Description of the package in the distribution
-    package_dir = {'Gnuplot' : ''},
-    packages = ['Gnuplot']
+    package_dir={'Gnuplot' : ''},
+    packages=['Gnuplot'],
     )
 
