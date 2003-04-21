b87f8a5afdd9644568ac598717d0aa4e031f923a
mhagger <mhagger@cc615063-760d-0410-8897-8c88c9ce4bef>
2003-04-21 09:44:09 +0000

    Changed license to LGPL.

--- gp_mac.py
+++ gp_mac.py
@@ -1,18 +1,10 @@
 # $Id$
 
-# Copyright (C) 1999 Michael Haggerty <mhagger@alum.mit.edu>
+# Copyright (C) 1999-2003 Michael Haggerty <mhagger@alum.mit.edu>
 # Thanks to Tony Ingraldi and Noboru Yamamoto for their contributions.
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or (at
-# your option) any later version.  This program is distributed in the
-# hope that it will be useful, but WITHOUT ANY WARRANTY; without even
-# the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-# PURPOSE.  See the GNU General Public License for more details; it is
-# available at <http://www.fsf.org/copyleft/gpl.html>, or by writing to
-# the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-# Boston, MA 02111-1307, USA.
+# This file is licensed under the GNU Lesser General Public License
+# (LGPL).  See LICENSE.txt for details.
 
 """gp_mac -- an interface to gnuplot for the Macintosh.
 
